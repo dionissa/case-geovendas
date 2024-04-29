d9816f576b134f1b75ae66b9dc1c60257ecf017d
dionissa <dionissarodrigo@gmail.com>
2024-04-29 19:33:30 -0300

    First Commit

---
+++ AtualizadorEstoque.php
@@ -0,0 +1,70 @@
+<?php
+
+class AtualizadorEstoque {
+    private $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function atualizarEstoqueDoArquivo($nomeArquivo) {
+        $json = file_get_contents($nomeArquivo);
+        $dadosEstoque = json_decode($json, true);
+        $this->atualizarEstoque($dadosEstoque);
+    }
+
+    public function atualizarEstoque(array $dadosEstoque) {
+        $verificaExistencia = "SELECT quantidade FROM estoque WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito";
+        $atualizaEstoque = "UPDATE estoque SET quantidade = quantidade + :quantidade WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito";
+        $insereEstoque = "INSERT INTO estoque (produto, cor, tamanho, deposito, data_disponibilidade, quantidade) VALUES (:produto, :cor, :tamanho, :deposito, :data_disponibilidade, :quantidade)";
+        
+        $stmtExistencia = $this->pdo->prepare($verificaExistencia);
+        $stmtAtualiza = $this->pdo->prepare($atualizaEstoque);
+        $stmtInsere = $this->pdo->prepare($insereEstoque);
+
+        $this->pdo->beginTransaction();
+
+        foreach ($dadosEstoque as $dados) {
+            $stmtExistencia->execute([
+                ':produto' => $dados['produto'],
+                ':cor' => $dados['cor'],
+                ':tamanho' => $dados['tamanho'],
+                ':deposito' => $dados['deposito']
+            ]);
+
+            $produtoExistente = $stmtExistencia->fetch(PDO::FETCH_ASSOC);
+
+            if ($produtoExistente) {
+                $stmtAtualiza->execute([
+                    ':produto' => $dados['produto'],
+                    ':cor' => $dados['cor'],
+                    ':tamanho' => $dados['tamanho'],
+                    ':deposito' => $dados['deposito'],
+                    ':quantidade' => $dados['quantidade']
+                ]);
+            } else {
+                $stmtInsere->execute([
+                    ':produto' => $dados['produto'],
+                    ':cor' => $dados['cor'],
+                    ':tamanho' => $dados['tamanho'],
+                    ':deposito' => $dados['deposito'],
+                    ':data_disponibilidade' => $dados['data_disponibilidade'],
+                    ':quantidade' => $dados['quantidade']
+                ]);
+            }
+        }
+
+        $this->pdo->commit();
+    }
+}
+
+$dsn = "mysql:host=geo-resolution-database-case-geo.h.aivencloud.com;port=25310;dbname=defaultdb";
+$usuario = "avnadmin";
+$senha = "AVNS_HLqV5j3z7uegvKzeyC4";
+
+$pdo = new PDO($dsn, $usuario, $senha);
+$atualizadorEstoque = new AtualizadorEstoque($pdo);
+$arquivoDados = 'data.json';
+$atualizadorEstoque->atualizarEstoqueDoArquivo($arquivoDados);
+
+?>
---
+++ README.md
@@ -0,0 +1,76 @@
+## Resolução de Case - Geovendas - PHP + MYSQL - Rodrigo Dionissa
+
+## Resolução privada
+
+## Atualizador de Estoque
+Este é um script PHP para atualizar o estoque de produtos em um banco de dados a partir de um arquivo JSON. Ele verifica se os produtos já existem no estoque e os atualiza ou adiciona novos produtos caso não existam.
+
+## Como funciona
+O script lê o conteúdo de um arquivo JSON fornecido.
+Decodifica os dados JSON em um array associativo PHP.
+Conecta-se ao banco de dados usando PDO.
+Itera sobre os dados do estoque e verifica se cada produto já existe no banco de dados.
+Se o produto existir, atualiza a quantidade disponível no estoque.
+Se o produto não existir, insere um novo registro no banco de dados.
+Finalmente, confirma as alterações no banco de dados.
+
+## Requisitos
+- PHP >= 5.6
+- Servidor MySQL
+- Extensão PDO MySQL habilitada
+
+## Como usar
+Clone o repositório ou baixe o arquivo PHP.
+Certifique-se de ter um arquivo JSON com os dados do estoque no mesmo diretório.
+Configure as credenciais do banco de dados no arquivo PHP.
+Execute o script PHP no terminal ou no navegador.
+Verifique o banco de dados para ver as atualizações no estoque.
+
+## Exemplo de data.json para teste:
+```json
+[
+{
+"produto": "10.01.0419",
+"cor": "00",
+"tamanho": "P",
+"deposito": "DEP1",
+"data_disponibilidade": "2023-05-01",
+"quantidade": 15
+},
+{
+"produto": "11.01.0568",
+"cor": "08",
+"tamanho": "P",
+"deposito": "DEP1",
+"data_disponibilidade": "2023-05-01",
+"quantidade": 2
+},
+{
+"produto": "11.01.0568",
+"cor": "08",
+"tamanho": "M",
+"deposito": "DEP1",
+"data_disponibilidade": "2023-05-01",
+"quantidade": 4
+},
+{
+"produto": "11.01.0568",
+"cor": "08",
+"tamanho": "G",
+"deposito": "1",
+"data_disponibilidade": "2023-05-01",
+"quantidade": 6
+},
+{
+"produto": "11.01.0568",
+"cor": "08",
+"tamanho": "P",
+"deposito": "DEP1",
+"data_disponibilidade": "2023-06-01",
+"quantidade": 8
+}
+]
+``` 
+
+## Configuração
+No arquivo AtualizadorEstoque.php, você pode ajustar as consultas SQL e as credenciais do banco de dados conforme necessário.
