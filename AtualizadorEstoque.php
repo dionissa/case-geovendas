d0f788b21d356cc0df1f333539ec7977c6ca08fe
dionissa <dionissarodrigo@gmail.com>
2024-04-29 21:09:40 -0300

    Update quantity when repeat a product with same caracteristics

--- AtualizadorEstoque.php
+++ AtualizadorEstoque.php
@@ -14,16 +14,16 @@ class AtualizadorEstoque {
     }
 
     public function atualizarEstoque(array $dadosEstoque) {
-        $verificaExistencia = "SELECT quantidade FROM estoque WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito";
-        $atualizaEstoque = "UPDATE estoque SET quantidade = quantidade + :quantidade WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito";
+        $verificaExistencia = "SELECT quantidade, data_disponibilidade FROM estoque WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito ORDER BY data_disponibilidade DESC LIMIT 1";
+        $atualizaEstoque = "UPDATE estoque SET quantidade = :quantidade, data_disponibilidade = :data_disponibilidade WHERE produto = :produto AND cor = :cor AND tamanho = :tamanho AND deposito = :deposito";
         $insereEstoque = "INSERT INTO estoque (produto, cor, tamanho, deposito, data_disponibilidade, quantidade) VALUES (:produto, :cor, :tamanho, :deposito, :data_disponibilidade, :quantidade)";
         
         $stmtExistencia = $this->pdo->prepare($verificaExistencia);
         $stmtAtualiza = $this->pdo->prepare($atualizaEstoque);
         $stmtInsere = $this->pdo->prepare($insereEstoque);
-
+    
         $this->pdo->beginTransaction();
-
+    
         foreach ($dadosEstoque as $dados) {
             $stmtExistencia->execute([
                 ':produto' => $dados['produto'],
@@ -31,16 +31,19 @@ class AtualizadorEstoque {
                 ':tamanho' => $dados['tamanho'],
                 ':deposito' => $dados['deposito']
             ]);
-
+    
             $produtoExistente = $stmtExistencia->fetch(PDO::FETCH_ASSOC);
-
+    
             if ($produtoExistente) {
+                $novaQuantidade = $dados['quantidade'];
+                $dataDisponibilidade = date('Y-m-d');
                 $stmtAtualiza->execute([
                     ':produto' => $dados['produto'],
                     ':cor' => $dados['cor'],
                     ':tamanho' => $dados['tamanho'],
                     ':deposito' => $dados['deposito'],
-                    ':quantidade' => $dados['quantidade']
+                    ':quantidade' => $novaQuantidade,
+                    ':data_disponibilidade' => $dataDisponibilidade
                 ]);
             } else {
                 $stmtInsere->execute([
@@ -53,9 +56,10 @@ class AtualizadorEstoque {
                 ]);
             }
         }
-
+    
         $this->pdo->commit();
     }
+    
 }
 
 $dsn = "mysql:host=geo-resolution-database-case-geo.h.aivencloud.com;port=25310;dbname=defaultdb";
